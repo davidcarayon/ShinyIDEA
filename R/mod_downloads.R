4f5edbf7ab451aa366ab72d3b76e7b775a59c9db
David Carayon <david.carayon@inrae.fr>
2020-12-14 12:51:13 +0100

    enhanced downloads and support for group

--- R/mod_downloads.R
+++ R/mod_downloads.R
@@ -31,25 +31,30 @@ dlmodule <- function(input, output, session) {
     
     content = function(file){
       
-      withProgress(message = "Votre fichier est en cours de production...",detail = "Merci de patienter quelques instants",{
-        
-        file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
-        
-        # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
-        knitting_dir <- file.path(tempdir(), "IDEATools_reports")
-        if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
-        file.copy(system.file("report/", package = "IDEATools"), knitting_dir, recursive = TRUE)
-        
-        diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
-                  export_type = "report", type = "single", quiet = TRUE, report_format = "pdf")
-        
-        incProgress(0.8)
-        
-        file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".pdf")), file)
-        
-        
-      })
+      progressSweetAlert(
+        session = session, id = "myprogress",
+        title = "Production du fichier en cours...",
+        display_pct = FALSE, value = 100
+      )
       
+      file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
+      
+      # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
+      knitting_dir <- file.path(tempdir(), "IDEATools_reports")
+      if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
+      
+      diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
+                export_type = "report", type = "single", quiet = TRUE, report_format = "pdf")
+      
+      
+      file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".pdf")), file)
+      
+      closeSweetAlert(session = session)
+      sendSweetAlert(
+        session = session,
+        title =" Fichier téléchargé !",
+        type = "success"
+      )
     }
     
   )
@@ -65,24 +70,31 @@ dlmodule <- function(input, output, session) {
     
     content = function(file){
       
-      withProgress(message = "Votre fichier est en cours de production...",detail = "Merci de patienter quelques instants",{
-        
-        file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
-        
-        # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
-        knitting_dir <- file.path(tempdir(), "IDEATools_reports")
-        if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
-        file.copy(system.file("report/", package = "IDEATools"), knitting_dir, recursive = TRUE)
-        
-        diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
-                  export_type = "report", type = "single", quiet = TRUE, report_format = "pptx")
-        
-        incProgress(0.8)
-        
-        file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".pptx")), file)
-        
-        
-      })
+      progressSweetAlert(
+        session = session, id = "myprogress_6",
+        title = "Production du fichier en cours...",
+        display_pct = FALSE, value = 100
+      )
+      
+      file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
+      
+      # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
+      knitting_dir <- file.path(tempdir(), "IDEATools_reports")
+      if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
+      
+      diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
+                export_type = "report", type = "single", quiet = TRUE, report_format = "pptx")
+      
+      incProgress(0.8)
+      
+      file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".pptx")), file)
+      
+      closeSweetAlert(session = session)
+      sendSweetAlert(
+        session = session,
+        title =" Fichier téléchargé !",
+        type = "success"
+      )
       
     }
     
@@ -98,24 +110,31 @@ dlmodule <- function(input, output, session) {
     
     content = function(file){
       
-      withProgress(message = "Votre fichier est en cours de production...",detail = "Merci de patienter quelques instants",{
-        
-        file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
-        
-        # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
-        knitting_dir <- file.path(tempdir(), "IDEATools_reports")
-        if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
-        file.copy(system.file("report/", package = "IDEATools"), knitting_dir, recursive = TRUE)
-        
-        diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
-                  export_type = "report", type = "single", quiet = TRUE, report_format = "docx")
-        
-        incProgress(0.8)
-        
-        file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".docx")), file)
-        
-        
-      })
+      progressSweetAlert(
+        session = session, id = "myprogress_3",
+        title = "Production du fichier en cours...",
+        display_pct = FALSE, value = 100
+      )
+      
+      file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
+      
+      # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
+      knitting_dir <- file.path(tempdir(), "IDEATools_reports")
+      if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
+      
+      diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
+                export_type = "report", type = "single", quiet = TRUE, report_format = "docx")
+      
+      
+      file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".docx")), file)
+      
+      closeSweetAlert(session = session)
+      sendSweetAlert(
+        session = session,
+        title =" Fichier téléchargé !",
+        type = "success"
+      )
+      
       
     }
     
@@ -131,24 +150,30 @@ dlmodule <- function(input, output, session) {
     
     content = function(file){
       
-      withProgress(message = "Votre fichier est en cours de production...",detail = "Merci de patienter quelques instants",{
-        
-        file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
-        
-        # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
-        knitting_dir <- file.path(tempdir(), "IDEATools_reports")
-        if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
-        file.copy(system.file("report/", package = "IDEATools"), knitting_dir, recursive = TRUE)
-        
-        diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
-                  export_type = "report", type = "single", quiet = TRUE, report_format = "odt")
-        
-        incProgress(0.8)
-        
-        file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".odt")), file)
-        
-        
-      })
+      progressSweetAlert(
+        session = session, id = "myprogress_4",
+        title = "Production du fichier en cours...",
+        display_pct = FALSE, value = 100
+      )
+      
+      file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
+      
+      # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
+      knitting_dir <- file.path(tempdir(), "IDEATools_reports")
+      if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
+      
+      diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
+                export_type = "report", type = "single", quiet = TRUE, report_format = "odt")
+      
+      
+      file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".odt")), file)
+      
+      closeSweetAlert(session = session)
+      sendSweetAlert(
+        session = session,
+        title =" Fichier téléchargé !",
+        type = "success"
+      )
       
     }
     
@@ -165,26 +190,34 @@ dlmodule <- function(input, output, session) {
     content = function(file){
       
       
-      withProgress(message = "Votre fichier est en cours de production...",detail = "Merci de patienter quelques instants",{
-        
-        outdir <- file.path(tempdir(),"Figures")
-        
-        file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
-        
-        diag_idea(input$files$datapath, output_directory = outdir,type = "single", export_type = "local", quiet = TRUE, prefix = file_name_short)
-        
-        ## Définition du chemin des fichiers à archiver
-        current_dir <- getwd()
-        setwd(file.path(outdir,Sys.Date()))
-        
-        fs <- list.files(file_name_short, recursive=TRUE, full.names = TRUE)
-        
-        # Export du zip
-        zip(zipfile = file, files = fs)
-        setwd(current_dir)
-        
-      })
+      progressSweetAlert(
+        session = session, id = "myprogress_5",
+        title = "Production du fichier en cours...",
+        display_pct = FALSE, value = 100
+      )
       
+      outdir <- file.path(tempdir(),"Figures")
+      
+      file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
+      
+      diag_idea(input$files$datapath, output_directory = outdir,type = "single", export_type = "local", quiet = TRUE, prefix = file_name_short)
+      
+      ## Définition du chemin des fichiers à archiver
+      current_dir <- getwd()
+      setwd(file.path(outdir,Sys.Date()))
+      
+      fs <- list.files(file_name_short, recursive=TRUE, full.names = TRUE)
+      
+      # Export du zip
+      zip(zipfile = file, files = fs)
+      setwd(current_dir)
+      
+      closeSweetAlert(session = session)
+      sendSweetAlert(
+        session = session,
+        title =" Fichier téléchargé !",
+        type = "success"
+      )
       
       
     }
@@ -201,24 +234,30 @@ dlmodule <- function(input, output, session) {
     
     content = function(file){
       
-      withProgress(message = "Votre fichier est en cours de production...",detail = "Merci de patienter quelques instants",{
-        
-        file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
-        
-        # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
-        knitting_dir <- file.path(tempdir(), "IDEATools_reports")
-        if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
-        file.copy(system.file("report/", package = "IDEATools"), knitting_dir, recursive = TRUE)
-        
-        diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
-                  export_type = "report", type = "single", quiet = TRUE, report_format = "xlsx")
-        
-        incProgress(0.8)
-        
-        file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".xlsx")), file)
-        
-        
-      })
+      progressSweetAlert(
+        session = session, id = "myprogress_6",
+        title = "Production du fichier en cours...",
+        display_pct = FALSE, value = 100
+      )
+      
+      file_name_short <- substr(basename(tools::file_path_sans_ext(input$files$name)), start = 1, stop = 10)
+      
+      # Defining a knitting dir in tempdir in case the user doesn't have all permissions in working dir
+      knitting_dir <- file.path(tempdir(), "IDEATools_reports")
+      if (!dir.exists(knitting_dir)) (dir.create(knitting_dir))
+      
+      diag_idea(input$files$datapath,output_directory = knitting_dir,prefix = file_name_short,
+                export_type = "report", type = "single", quiet = TRUE, report_format = "xlsx")
+      
+      
+      file.copy(file.path(knitting_dir,Sys.Date(),file_name_short,paste0("Rapport_individuel_",file_name_short,".xlsx")), file)
+      
+      closeSweetAlert(session = session)
+      sendSweetAlert(
+        session = session,
+        title =" Fichier téléchargé !",
+        type = "success"
+      )
       
     }
     
